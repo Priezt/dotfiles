9e196e30333cb3791f16a5c4f65ece4b1574d640
Priezt <vegetabird@gmail.com>
2016-10-01 15:36:10 +0800

    add bash

---
+++ bash/alias.sh
@@ -0,0 +1,26 @@
+if uname -a | grep -i freebsd > /dev/null
+then
+	alias ls='ls -G'
+else
+	alias ls='ls --color=auto'
+fi
+alias ll='ls -l'
+alias grep='grep --color=auto'
+alias vi=vim
+alias cd..2='cd ../..'
+alias cd..3='cd ../../..'
+alias cd..4='cd ../../../..'
+alias cd..5='cd ../../../../..'
+alias cd..6='cd ../../../../../..'
+alias cd..7='cd ../../../../../../..'
+alias cd..8='cd ../../../../../../../..'
+alias cd..9='cd ../../../../../../../../..'
+alias newest='find . -type f -exec ls -1lt "{}" + | head '
+alias short='egrep -v ".{120}"'
+
+alias json="python -mjson.tool"
+alias gitsearch='git rev-list --all | xargs git grep -F'
+
+cls() { cd "$1" && ls; }
+alias ipinfo="curl ifconfig.me && curl ifconfig.me/host"
+
---
+++ bash/completion.sh
@@ -0,0 +1,26 @@
+#complete -W "$(rake -T | awk '{print $2}')" rake
+
+export COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
+
+function _rakecomplete() {
+	local tasks=`rake -T | awk '{print $2}'`
+	COMPREPLY=($(compgen -W "${tasks}" -- ${COMP_WORDS[COMP_CWORD]}))
+	return 0
+}
+
+complete -o default -o nospace -F _rakecomplete rake
+_make_variables()
+{
+	local cur=${COMP_WORDS[COMP_CWORD]}
+	local var=$(make -p Makefile | egrep '^[a-zA-Z0-9_ ]+:' | sed 's/ .*//'| sed 's/:.*//')
+	compopt -o nospace
+	COMPREPLY=( $(compgen -W "${var}" -- ${cur}) )
+}
+complete -F _make_variables make
+
+complete -c which
+complete -c man
+complete -c sudo
+
+#export IFS=" "
+#complete -W "submit sync integrate edit revert" p4 # this shit will prevent p4 from filename completion
