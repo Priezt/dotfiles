79304773cfc65270f10a416a5ee819616ccd2f5f
Priezt <vegetabird@gmail.com>
2023-01-16 14:09:19 +0800

    vue template

---
+++ vim/filetype-templates/template.vue
@@ -0,0 +1,23 @@
+<template>
+</template>
+<style>
+</style>
+<script>
+export default {
+	name: 'module_name',
+	data () {
+		return {
+		}
+	},
+	props: {
+	},
+	computed: {
+	},
+	components: {
+	},
+	methods: {
+	},
+	async mounted(){
+	},
+}
+</script>
